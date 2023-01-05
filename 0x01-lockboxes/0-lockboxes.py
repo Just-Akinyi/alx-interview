5736aa93122abb9bd075645702c26dd4995a954c
Just-Akinyi <justakinyi1@gmail.com>
2023-01-05 22:31:49 +0300

    lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -9,27 +9,24 @@ def canUnlockAll(boxes):
     returns:
         True or False
     '''
-    # Check if the input is valid
-    if not boxes:
-        return False
-    # Set to keep track of which boxes have been opened
-    opened = set()
-    # Queue for the keys
-    keys = []
-    # Add the keys in the first box to the queue
-    # and mark the first box as opened
-    keys.extend(boxes[0])
-    opened.add(0)
-    # BFS loop
-    while keys:
-        # Get the next key
-        key = keys.pop(0)
-        # Check if the key is a valid box index
-        # and if the box has not been opened
-        if 0 <= key < len(boxes) and key not in opened:
+    # Set of boxes that have been visited
+    visited = set()
+
+    # Queue for boxes that need to be checked
+    queue = [0]
+
+    # While there are boxes in the queue
+    while queue:
+        # Get the next box to check
+        box = queue.pop(0)
+
+        # If the box has not been visited
+        if box not in visited:
+            # Mark the box as visited
+            visited.add(box)
+
             # Add the keys in the box to the queue
-            keys.extend(boxes[key])
-            # Mark the box as opened
-            opened.add(key)
-    # Return True if all boxes have been opened, else return False
-    return len(opened) == len(boxes)
+            queue.extend(boxes[box])
+
+    # Return whether all boxes have been visited
+    return len(visited) == len(boxes)
